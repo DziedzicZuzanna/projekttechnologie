209dee850e3058c0329058337283b2852635c875
DziedzicZuzanna <149088453+DziedzicZuzanna@users.noreply.github.com>
2023-10-26 17:20:33 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,4 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
-
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR-XGDj8-7qJ6KYHteXQq434zEJrVoBY5BFY6oQv0vc8p6VZLeaZCY6OqKWC-eBo3JtSMdcK7ZH0p3j/pubhtml'
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
